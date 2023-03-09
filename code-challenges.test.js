a4dbd2eb39f25d861545fd797f59eefaa130401a
JoelCarr619 <JoelCarr619@gmail.com>
2023-03-09 14:37:25 -0800

    update to the update code challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -135,7 +135,7 @@ const accumulateSum = (arr) => {
   let result = []
   let sum = 0
 
-  for (let i = 0; i < arr.length; i+++) {
+  for (let i = 0; i < arr.length; i++) {
     sum += arr[i]
     result.push(sum)
   }
